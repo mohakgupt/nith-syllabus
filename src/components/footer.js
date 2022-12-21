30f278120f09bee3477d3393cea70ba7d94f271c
Mohak Gupta <100181077+mohakgupt@users.noreply.github.com>
2022-12-21 13:11:25 +0530

    added star btn

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,10 +1,11 @@
 import { Typography } from "@mui/material";
+import GitHubButton from 'react-github-btn';
 
 export default function Footer(){
     return(
         <Typography sx={{height: "0.5em",color: 'gray', textAlign: 'center',width: "100%", fontSize:"10px", position:'absolute', bottom: 0}}>
             <span style={{paddingTop: "10px"}}>Made with ❤️ by <a href="https://github.com/mohakgupt" style={{textDecoration: 'none', color: 'gray'}}>mohakgupt</a></span><br/>
-            <a style={{display:'block'}} class="github-button" href="https://github.com/mohakgupt/nith-syllabus" data-icon="octicon-star" data-show-count="true" aria-label="Star mohakgupt/nith-syllabus on GitHub">Star</a>
+            <GitHubButton href="https://github.com/mohakgupt/nith-syllabus" data-icon="octicon-star" data-show-count="true" aria-label="Star mohakgupt/nith-syllabus on GitHub">Star</GitHubButton>
             <div id="visits"></div>
         </Typography>
     )
