420b5ada9e99bd82e627b68b43afe59b2053a261
mohakgupt <thisismohak@gmail.com>
2023-01-18 21:06:48 +0530

    header

--- src/components/header.js
+++ src/components/header.js
@@ -2,7 +2,7 @@ import {Typography} from '@mui/material';
 
 export default function Header(){
     return(
-        <Typography variant='h3' sx={{textAlign: 'center', background: "linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(71,77,199,1) 42%, rgba(0,212,255,1) 100%);", backgroundClip: "text", color: "transparent"  }} gutterBottom>
+        <Typography variant='h4' sx={{textAlign: 'center', background: "linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(71,77,199,1) 42%, rgba(0,212,255,1) 100%);", backgroundClip: "text", color: "transparent"  }} gutterBottom>
             NITH SYLLABUS
         </Typography>
     )
